201fbc725918048fdb223e73cfb7d5635a0c0e33
Dhruv N Patel <dhruv@weam.ai>
2025-09-09 17:04:48 +0530

    Add ai-doc and foloup solution in main branch

--- nodejs/src/config/solutionconfig.js
+++ nodejs/src/config/solutionconfig.js
@@ -5,28 +5,28 @@ const SOLUTION_CONFIGS = {
         imageName: 'ai-doc-editor-img',
         containerName: 'ai-doc-editor-container',
         port: '3002',
-        branchName: 'development',
+        branchName: 'main',
         installType: 'docker', // docker or docker-compose
         envFile: 'env.example'
     },
-    'seo-content-gen': {
-        repoUrl: 'https://github.com/devweam-ai/seo-content-gen.git',
-        repoName: 'seo-content-gen',
-        imageName: 'seo-content-gen-img',
-        containerName: 'seo-content-gen-container',
-        port: '3003',
-        branchName: 'opensource-deployment',
-        installType: 'docker-compose', // docker or docker-compose
-        envFile: null, // No env file needed for docker-compose
-        additionalPorts: ['9001', '9002', '9003'] // Additional ports for docker-compose services
-    },
+    // 'seo-content-gen': {
+    //     repoUrl: 'https://github.com/devweam-ai/seo-content-gen.git',
+    //     repoName: 'seo-content-gen',
+    //     imageName: 'seo-content-gen-img',
+    //     containerName: 'seo-content-gen-container',
+    //     port: '3003',
+    //     branchName: 'opensource-deployment',
+    //     installType: 'docker-compose', // docker or docker-compose
+    //     envFile: null, // No env file needed for docker-compose
+    //     additionalPorts: ['9001', '9002', '9003'] // Additional ports for docker-compose services
+    // },
     'followup': {
         repoUrl: 'https://github.com/devweam-ai/foloup.git',
         repoName: 'foloup',
         imageName: 'foloup-img',
         containerName: 'foloup-container',
         port: '4000',
-        branchName: 'devops',
+        branchName: 'main',
         installType: 'docker-compose', // docker or docker-compose
         envFile: '.env.example', // Has .env.example file that needs to be converted to .env
         additionalPorts: [] // No additional ports specified
